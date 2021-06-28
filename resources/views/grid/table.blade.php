06b0a65ee3e49b7206f051620f343bf8d40be6b9
Софья <SVFilimonova@cbit.ru>
2021-06-28 10:16:11 +0700

    fix: news11

--- resources/views/grid/table.blade.php
+++ resources/views/grid/table.blade.php
@@ -46,7 +46,7 @@
                 @endif
                 @foreach($grid->rows() as $row)
                     {{
-dd($grid->model()->path())
+dd($grid->model()->path()->getOriginalModel())
 }}
                 @if(1)
                 <tr onclick="window.location.replace(location.href+'/'+{{$row->column('id')}}+'/edit')" {!! $row->getRowAttributes() !!}>
